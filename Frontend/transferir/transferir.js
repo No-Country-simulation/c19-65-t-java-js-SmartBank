45637f57a6c205daf3006b0349c7f6ce8fca9f15
Nicolas Pacheco N <nicolas.pachecon@outlook.com>
2024-07-15 22:02:01 -0400

    primera version de transferir.html

---
+++ Frontend/transferir/transferir.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("bankForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const fullName = document.getElementById("fullName").value;
+    const dni = document.getElementById("dni").value;
+    const bank = document.getElementById("bank").value;
+    const accountType = document.getElementById("accountType").value;
+    const amount = document.getElementById("amount").value;
+    const securityKey = document.getElementById("securityKey").value;
+
+    if (fullName && dni && bank && accountType && amount && securityKey) {
+      alert(
+        `Nombre y Apellido: ${fullName}\nDNI o RUT: ${dni}\nBanco: ${bank}\nTipo de Cuenta: ${accountType}\nMonto: ${amount}\nClave de Seguridad: ${securityKey}`
+      );
+    } else {
+      alert("Por favor, complete todos los campos requeridos.");
+    }
+  });
