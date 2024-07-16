caf0c9950b61053c6ae0a5d4e758da1f7a9759c3
edison <edison_20_04@hotmail.com>
2024-07-16 14:57:38 -0500

    Función listado de cuentas

--- Frontend/dashboard/dashboard.js
+++ Frontend/dashboard/dashboard.js
@@ -20,19 +20,31 @@ const $promociones = document.querySelector('#promociones')
 const $crearCuenta = document.querySelector('#crearCuenta')
 
 // Info del backend
-const accountList = {
-  '1234': {tipoCuenta: 'Cta Ahorros', saldo:450000},
-  '8888': {tipoCuenta: 'Cta Corriente', saldo:0},
+const accountsDetails = {
+  '1234': {tipoCuenta: 'Cuenta Ahorros', saldo:450000},
+  '8888': {tipoCuenta: 'Cuenta Corriente', saldo:0}
 }
 const invList = []
 
 //
 
 /**
- * todo - Llenar lista de cuentas
  * todo - Funcionalidad Saldo
  * todo - Actualizar link "Ver resumen"
  * todo - Funcionalidad Inversiones
  */
+
+// Funcion, actualización listado de cuentas
+const accountsKeys = Object.keys(accountsDetails)
+if(accountsKeys.length !== 0) {
+  const htmlAccountOpts = accountsKeys.map(account => {
+    const { tipoCuenta } = accountsDetails[account]
+    return `<option value="${account}">${account} - ${tipoCuenta}</option>`
+  })
+  $accountsList.innerHTML = htmlAccountOpts
+} else {
+  $accountsList.innerHTML = `<option value="">No tienes cuentas asociadas</option>`
+  $accountsList.setAttribute('disabled', 'true')
+}
 
 
