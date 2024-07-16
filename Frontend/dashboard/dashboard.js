de002e97c0dc9076508c555779560a1fd891862c
edison <edison_20_04@hotmail.com>
2024-07-15 20:39:27 -0500

    Declaración variables funcionalidad Dashboard

--- Frontend/dashboard/dashboard.js
+++ Frontend/dashboard/dashboard.js
@@ -1,2 +1,38 @@
 import '@components/navBar.js'
 import '@styles/style.css'
+
+// Elementos DOM
+const $accountsList = document.querySelector('#accountsList')
+
+const $saldo = document.querySelector('#saldo')
+const $saldoActivoIcn = document.querySelector('#saldo-activo')
+const $saldoOcultoIcn = document.querySelector('#saldo-oculto')
+
+const $btn_Movimientos = document.querySelector('#lnk_Movimientos')
+
+const $invGraph = document.querySelector('#invGraph')
+const $noInvMssg = document.querySelector('#noInvMssg')
+
+const $QR = document.querySelector('#QR')
+const $prestamos = document.querySelector('#prestamos')
+const $transferir = document.querySelector('#transferir')
+const $promociones = document.querySelector('#promociones')
+const $crearCuenta = document.querySelector('#crearCuenta')
+
+// Info del backend
+const accountList = {
+  '1234': {tipoCuenta: 'Cta Ahorros', saldo:450000},
+  '8888': {tipoCuenta: 'Cta Corriente', saldo:0},
+}
+const invList = []
+
+//
+
+/**
+ * todo - Llenar lista de cuentas
+ * todo - Funcionalidad Saldo
+ * todo - Actualizar link "Ver resumen"
+ * todo - Funcionalidad Inversiones
+ */
+
+
