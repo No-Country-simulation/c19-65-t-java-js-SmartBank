14e84c79b660209f7ec2bd4ee85108cd3e4df3e4
Mauro Cicchelli <mauromiramar@gmail.com>
2024-07-18 10:42:42 -0300

    Modificando detalles de la vista y agregando funcionalidad mostrar contraseña

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,2 +1,20 @@
-import '@components/navBar.js'
 import '@styles/style.css'
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('passwordInput'); // Cambio aquí
+    const showIcon = document.getElementById('showIcon');
+    const hideIcon = document.getElementById('hideIcon');
+    
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      showIcon.classList.add('hidden');
+      hideIcon.classList.remove('hidden');
+    } else {
+      passwordInput.type = 'password';
+      showIcon.classList.remove('hidden'); // Corrección: 'show' a 'hidden'
+      hideIcon.classList.add('hidden'); // Corrección: 'show' a 'hidden'
+    }
+}
+
+// Exponer la función al ámbito global
+window.togglePasswordVisibility = togglePasswordVisibility;
