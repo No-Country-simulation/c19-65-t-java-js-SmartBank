e2cce47898233aa9bc703bebf8796b0d9bdd8fa8
edison <edison_20_04@hotmail.com>
2024-07-18 20:01:21 -0500

    Responsive (Dashboard, header y navBar)

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -1,5 +1,5 @@
 document.querySelector('#header').innerHTML = `
-  <header class='w-full bg-root-header h-12 text-white flex py-1 px-4'>
+  <header class='w-screen bg-root-header h-12 text-white flex py-1 px-4'>
     <img src="/SmartBank.svg" alt="SmartBank Logo">
   </header>
 `
