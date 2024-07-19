0c478a4ab59899258c5bfb0283543005b3c63a20
Mauro Cicchelli <mauromiramar@gmail.com>
2024-07-19 10:46:36 -0300

    Centrando contenedores del overlay

--- Frontend/main.js
+++ Frontend/main.js
@@ -38,26 +38,26 @@ document.querySelector('#login').addEventListener('submit', async (e) => {
   console.log(resp) 
 })
 
-//* 
-// Obtener los elementos por ID
-const registrarseBtn = document.getElementById('registrarseBtn'); // Asegúrate de que este es el ID correcto para el botón "Registrarse"
-const ingresarBtn = document.getElementById('ingresarBtn'); // Asegúrate de que este es el ID correcto para el botón "Ingresar"
+//*!  Funciones para mostrar y ocultar los overlays
+
+const registrarseBtn = document.getElementById('registrarseBtn'); 
+const ingresarBtn = document.getElementById('ingresarBtn'); 
 const overlay1 = document.getElementById('overlay-1');
 const overlay2 = document.getElementById('overlay-2');
 
-// Función para mostrar overlay-2 y ocultar overlay-1
+//* Función para mostrar overlay-2 y ocultar overlay-1
 function mostrarOverlay2() {
   overlay1.style.display = 'none';
   overlay2.style.display = 'block';
 }
 
-// Función para mostrar overlay-1 y ocultar overlay-2
+//* Función para mostrar overlay-1 y ocultar overlay-2
 function mostrarOverlay1() {
   overlay1.style.display = 'block';
   overlay2.style.display = 'none';
 }
 
-// Agregar manejadores de eventos
+//* Agregar manejadores de eventos
 registrarseBtn.addEventListener('click', mostrarOverlay2);
 ingresarBtn.addEventListener('click', mostrarOverlay1);
 
