c579f668bc4ac0eb5b4cf713c0b8da4cb5551660
edison <edison_20_04@hotmail.com>
2024-07-19 20:48:51 -0500

    Login & SignUp Validation

--- Frontend/main.js
+++ Frontend/main.js
@@ -4,6 +4,10 @@ import '@components/header.js'
 import { logIn, signUp } from "@services/userService"
 import { deleteAllStorage } from "@services/storageService"
 
+// * Reset SessionStorage
+deleteAllStorage()
+
+// * Listeners de formularios
 document.querySelector('#login').addEventListener('submit', async (e) => {
   e.preventDefault()
   const { nombre: name, contraseña: password } = Object.fromEntries(new FormData(e.target))
@@ -12,6 +16,7 @@ document.querySelector('#login').addEventListener('submit', async (e) => {
   // ! Acción en caso de fallar el login
   // ! {response: false, message: '401 - El usuario no existe'}
   console.log(resp) 
+  // showErrorMessage(input, 'message')
 })
 
 document.querySelector('#signup').addEventListener('submit', async (e) => {
@@ -19,16 +24,16 @@ document.querySelector('#signup').addEventListener('submit', async (e) => {
   console.log('signup')
   const { nombre, apellido, dni, fechaNacimiento, email, telefono, direccion, passw1, passw2} = Object.fromEntries(new FormData(e.target))
 
-  if(passw1 === passw2) {
-    // TODO - Conexion con el backend
-    // const resp = await signUp({ nombre, apellido, dni, fechaNacimiento, email, telefono, direccion, passw1, passw2})
-  }
+  // TODO - Conexion con el backend
+  // const resp = await signUp({ nombre, apellido, dni, fechaNacimiento, email, telefono, direccion, passw1, passw2})
   
-  // ! Acción en caso de fallar: Usuario ya existe || passw1 != passw2
+  // ! Acción en caso de fallar: Usuario ya existe
   // ! {response: false, message: 'Fallo en el registro'}
-  // console.log(resp) 
+  // console.log(resp)
+  // showErrorMessage(input, 'message')
 })
 
+// * Funcionalidad ver/ocultar password
 function togglePasswordVisibility() {
   const passwordInput = document.getElementById('passwordInput'); // Cambio aquí
   const showIcon = document.getElementById('showIcon');
@@ -47,9 +52,6 @@ function togglePasswordVisibility() {
 
 window.togglePasswordVisibility = togglePasswordVisibility;
 
-// * Reset SessionStorage
-deleteAllStorage()
-
 //* Funciones para mostrar y ocultar los overlays
 
 const registrarseBtn = document.getElementById('registrarseBtn'); 
@@ -57,47 +59,36 @@ const ingresarBtn = document.getElementById('ingresarBtn');
 const overlay1 = document.getElementById('overlay-1');
 const overlay2 = document.getElementById('overlay-2');
 
-//* Función para mostrar overlay-2 y ocultar overlay-1
+//? Función para mostrar overlay-2 y ocultar overlay-1
 function mostrarOverlay2() {
   overlay1.style.display = 'none';
   overlay2.style.display = 'block';
 }
 
-//* Función para mostrar overlay-1 y ocultar overlay-2
+//? Función para mostrar overlay-1 y ocultar overlay-2
 function mostrarOverlay1() {
   overlay1.style.display = 'block';
   overlay2.style.display = 'none';
 }
 
-//* Agregar manejadores de eventos
+//? Agregar manejadores de eventos
 registrarseBtn.addEventListener('click', mostrarOverlay2);
 ingresarBtn.addEventListener('click', mostrarOverlay1);
 
 
-//! Validar formulario de registro
+//* Live Validation 
 
-document.addEventListener('DOMContentLoaded', function() {
-  const submitButton = document.querySelector('button[type="submit"]');
-  const inputs = document.querySelectorAll('input');
-  const password1 = document.getElementById('passw1');
-  const password2 = document.getElementById('passw2');
-  const emailInput = document.querySelector('input[type="email"]');
-  const dniInput = document.getElementById('dni');
-  const termsCheckbox = document.getElementById('agree');
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
 
-  submitButton.disabled = true;
+function validateDNI(dni) {
+  const regex = /^\d{7,8}$/;
+  return regex.test(dni);
+}
 
-  function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  }
-
-  function validateDNI(dni) {
-    const regex = /^\d{7,8}$/;
-    return regex.test(dni);
-  }
-
-  function validatePasswords() {
+function validatePasswords() {
     if (password1.value === password2.value) {
       password2.setCustomValidity('');
       removeErrorMessage(password2);
@@ -105,64 +96,91 @@ document.addEventListener('DOMContentLoaded', function() {
       password2.setCustomValidity('Las contraseñas no coinciden');
       showErrorMessage(password2, 'Las contraseñas no coinciden');
     }
+}
+
+function validateInput(input) {
+  const isEmail = input.type === 'email'
+  const isDNI = input.id === 'regDni'
+  const isCheckbox = input.id === 'agree'
+  const isEmpty = !input.value.trim()
+  const isInvalidEmail = isEmail && !validateEmail(input.value)
+  const isInvalidDNI = isDNI && !validateDNI(input.value)
+  const isNotChecked = isCheckbox && !input.checked
+  if (isEmpty || isInvalidEmail || isInvalidDNI || isNotChecked) {
+    const errorMessage = isEmpty || isNotChecked
+      ? 'Esta información es necesaria' 
+      : isInvalidEmail 
+        ? 'Ingrese un email válido' 
+        : 'Ingrese un DNI válido';
+    showErrorMessage(input, errorMessage);
+  } else if (input.id !== 'passw2')
+      removeErrorMessage(input)
+    else
+      validatePasswords()
+}
+
+function validateForm(input, submitBtn, inputs) {
+  validateInput(input)
+  const isValid = Array.from(inputs).every( (input) => {
+    const isEmail = input.type === 'email'
+    const isDNI = input.id === 'regDni'
+    const isCheckbox = input.id === 'agree'
+    const isEmpty = !input.value.trim()
+    const isInvalidEmail = isEmail && !validateEmail(input.value)
+    const isInvalidDNI = isDNI && !validateDNI(input.value)
+    const isNotChecked = isCheckbox && !input.checked
+    // console.log({isEmpty, isInvalidEmail, isInvalidDNI, isNotChecked})
+    if (isEmpty || isInvalidEmail || isInvalidDNI || isNotChecked) return false
+    else if (input.id !== 'passw2') return true
+    else if (password1.value === password2.value) return true
+    else return false
+  })
+  submitBtn.disabled = !isValid;
+}
+
+// ? Mensajes de error
+function showErrorMessage(input, message) {
+  let error = input.nextElementSibling;
+  if (!error || !error.classList.contains('error-message')) {
+    error = document.createElement('span');
+    error.className = 'error-message';
+    error.style.color = 'red';
+    error.textContent = message;
+    input.parentNode.insertBefore(error, input.nextSibling);
   }
+  input.style.borderColor = 'red';
+}
 
-  function validateInput(input) {
-    const isEmail = input.type === 'email';
-    const isDNI = input === dniInput;
-    const isEmpty = !input.value.trim();
-    const isInvalidEmail = isEmail && !validateEmail(input.value);
-    const isInvalidDNI = isDNI && !validateDNI(input.value);
-
-    if (isEmpty || isInvalidEmail || isInvalidDNI) {
-      const errorMessage = isEmpty ? 'Esta información es necesaria' : isInvalidEmail ? 'Ingrese un email válido' : 'Ingrese un DNI válido';
-      showErrorMessage(input, errorMessage);
-      return false;
-    } else {
-      removeErrorMessage(input);
-      return true;
-    }
+function removeErrorMessage(input) {
+  let error = input.nextElementSibling;
+  if (error && error.classList.contains('error-message')) {
+    error.parentNode.removeChild(error);
   }
+  input.style.borderColor = '';
+}
 
-  function validateForm() {
-    const isValid = Array.from(inputs).every(validateInput);
-    submitButton.disabled = !isValid;
-  }
+//* Live Validation - Formulario Registro
+const signUpBtn = document.querySelector('#signup button')
+const signUpInputs = document.querySelectorAll('#signup input');
+const password1 = document.getElementById('passw1');
+const password2 = document.getElementById('passw2');
 
-  function showErrorMessage(input, message) {
-    let error = input.nextElementSibling;
-    if (!error || !error.classList.contains('error-message')) {
-      error = document.createElement('span');
-      error.className = 'error-message';
-      error.style.color = 'red';
-      error.textContent = message;
-      input.parentNode.insertBefore(error, input.nextSibling);
-    }
-    input.style.borderColor = 'red';
-  }
+signUpBtn.disabled = true
 
-  function removeErrorMessage(input) {
-    let error = input.nextElementSibling;
-    if (error && error.classList.contains('error-message')) {
-      error.parentNode.removeChild(error);
-    }
-    input.style.borderColor = '';
-  }
+signUpInputs.forEach(input => {
+  input.addEventListener('blur', () => validateForm(input, signUpBtn, signUpInputs))
+  input.addEventListener('input', () => validateForm(input, signUpBtn, signUpInputs))
+})
 
-  inputs.forEach(input => {
-    input.addEventListener('blur', () => validateInput(input));
-    input.addEventListener('input', () => validateInput(input));
-  });
 
-  password1.addEventListener('input', validatePasswords);
-  password2.addEventListener('input', validatePasswords);
+//* Live Validation - Formulario Login
 
-  const form = document.querySelector('form');
-  form.addEventListener('submit', function(event) {
-    validateForm();
-    if (submitButton.disabled) {
-      event.preventDefault();
-    }
-  });
-});
+const loginBtn = document.querySelector('#login button')
+const loginInputs = document.querySelectorAll('#login input')
 
+loginBtn.disabled = true
+
+loginInputs.forEach(input => {
+  input.addEventListener('blur', () => validateForm(input, loginBtn, loginInputs))
+  input.addEventListener('input', () => validateForm(input, loginBtn, loginInputs))
+})
