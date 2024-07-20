eca2f9e503ea9c02adc5a99fb8cb76338d42f385
edison <edison_20_04@hotmail.com>
2024-07-20 11:49:24 -0500

    remove extra padding header

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -2,6 +2,6 @@ import '@helpers/logos'
 
 document.querySelector('#header').innerHTML = `
   <div class='w-screen bg-root-header h-12 text-white flex py-1 px-4'>
-    <div class='smartBankSVG px-4'></div>
+    <div class='smartBankSVG'></div>
   </div>
 `
