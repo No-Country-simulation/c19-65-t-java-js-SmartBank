e45fb73bebcd36112d9ef2260d67ac7dbfb666f1
edison <edison_20_04@hotmail.com>
2024-07-21 08:50:51 -0500

    fix responsive lightbox

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -8,7 +8,7 @@ document.querySelector('#header').innerHTML = `
     <input id="cerrar-modal" type="radio" name="modal" class="hidden"/>
     <label for="cerrar-modal" class="hidden absolute bg-rose-500 w-6 h-6 rounded-full transition-all duration-500 z-30 text-white font-bold cursor-pointer text-center top-4 right-4">X</label>
     <div id="modal" class='bg-black/75 text-white fixed top-[-100vh] left-0 h-screen w-screen z-20 transition-all duration-500 flex items-center justify-center'>
-      <div class="bg-root-popup w-3/5 h-96 max-w-screen-sm rounded-3xl flex items-center border border-b-slate-400 p-5">
+      <div class="bg-root-popup w-11/12 sm:w-3/5 h-96 max-w-screen-sm rounded-3xl flex items-center border border-b-slate-400 p-5">
         <p id="modalmsg" class="w-full text-3xl font-semibold text-center"></p>
       </div>
     </div>
